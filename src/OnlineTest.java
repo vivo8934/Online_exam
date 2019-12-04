0727595ec44fea919a58c3b58f20c35af67aa15a
zubairadams <mfundo8934@gmail.com>
2019-12-04 17:59:47 +0200

    added a modified online about the project

--- src/OnlineTest.java
+++ src/OnlineTest.java
@@ -27,17 +27,17 @@ public  class OnlineTest extends JFrame implements ActionListener {
         b2.addActionListener(this);
         add(b1);add(b2);
         set();
-        l.setBounds(30,40,450,20);
-        jb[0].setBounds(50,80,100,20);
-        jb[1].setBounds(50,110,100,20);
-        jb[2].setBounds(50,140,100,20);
-        jb[3].setBounds(50,170,100,20);
+        l.setBounds(30,40,650,20);
+        jb[0].setBounds(50,80,300,20);
+        jb[1].setBounds(50,110,300,20);
+        jb[2].setBounds(50,140,300,20);
+        jb[3].setBounds(50,170,300,20);
         b1.setBounds(100,240,100,30);
         b2.setBounds(270,240,100,30);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        setLocation(250,100);
+        setLocation(250,300);
         setVisible(true);
         setSize(600,350);
     }
@@ -70,6 +70,9 @@ public  class OnlineTest extends JFrame implements ActionListener {
             if(current==9)
                 b2.setText("Result");
             setVisible(false);
+
+
+
             setVisible(true);
         }
         for (int i=0,y=1;i<x;i++,y++){
@@ -85,8 +88,9 @@ public  class OnlineTest extends JFrame implements ActionListener {
         }
 
         if(e.getActionCommand().equals("Result")){
-            if (check())
+            if (check()) {
                 count += 1;
+            }
             current++;
             JOptionPane.showMessageDialog(this, "correct ans= " + count);
             System.exit(0);
@@ -97,32 +101,32 @@ public  class OnlineTest extends JFrame implements ActionListener {
         jb[4].setSelected(true);
         if(current==0)
         {
-            l.setText("Que1: Which one among these is not a primitive datatype?");
+            l.setText("Question_1: Which one among these is not a primitive datatype?");
             jb[0].setText("int");jb[1].setText("Float");jb[2].setText("boolean");jb[3].setText("char");
         }
         if(current==1)
         {
-            l.setText("Que2: Which class is available to all the class automatically?");
-            jb[0].setText("Swing");jb[1].setText("Applet");jb[2].setText("Object");jb[3].setText("ActionEvent");
+            l.setText("Question_2: Which of the following statements are true?");
+            jb[0].setText("Runtime exceptions are checked exceptions.");jb[1].setText("Runtime exceptions are unchecked exceptions");jb[2].setText("You can declare only checked exceptions");jb[3].setText("You can handle only Exception subclasses");
         }
         if(current==2)
         {
-            l.setText("Que3: Which package is directly available to our class without importing it?");
-            jb[0].setText("swing");jb[1].setText("applet");jb[2].setText("net");jb[3].setText("lang");
+            l.setText("Question_3:When are you required to use a finally block in a regular try statement not a try-with-resources?");
+            jb[0].setText("Never");jb[1].setText("When the program code cannot exit on its own.");jb[2].setText("When there are no catch blocks in a try statement.");jb[3].setText("When there is one catch block in a try statement.");
         }
         if(current==3)
         {
-            l.setText("Que4: String class is defined in which package?");
-            jb[0].setText("lang");jb[1].setText("Swing");jb[2].setText("Applet");jb[3].setText("awt");
+            l.setText("Question_4: Which of the following lines will compile?");
+            jb[0].setText("char d=\"d\"");jb[1].setText("float f=3.1415;");jb[2].setText("byte b=257;");jb[3].setText("boolean isPresent=true;");
         }
         if(current==4)
         {
-            l.setText("Que5: Which institute is best for java coaching?");
-            jb[0].setText("Utek");jb[1].setText("Aptech");jb[2].setText("SSS IT");jb[3].setText("jtek");
+            l.setText("Question_5: Which of the following statements will compile?");
+            jb[0].setText("int myArray[];");jb[1].setText("int myArray[5];");jb[2].setText("t myArray[5] = {1,2}");jb[3].setText("myArray[] = {1,2,3,4,5};");
         }
         if(current==5)
         {
-            l.setText("Que6: Which one among these is not a keyword?");
+            l.setText("Question_6: Which one among these is not a keyword?");
             jb[0].setText("class");jb[1].setText("int");jb[2].setText("get");jb[3].setText("if");
         }
         if(current==6)
@@ -133,20 +137,20 @@ public  class OnlineTest extends JFrame implements ActionListener {
         }
         if(current==7)
         {
-            l.setText("Que8: which one among these is not a function of Object class?");
-            jb[0].setText("toString");jb[1].setText("finalize");jb[2].setText("equals");
-            jb[3].setText("getDocumentBase");
+            l.setText("Question_8: Which one is true?");
+            jb[0].setText("main method cannot be overloaded");jb[1].setText("code cant compile without main method");jb[2].setText("equals same as main method");
+            jb[3].setText("tostring and main share same package");
         }
         if(current==8)
         {
-            l.setText("Que9: which function is not present in Applet class?");
-            jb[0].setText("init");jb[1].setText("main");jb[2].setText("start");jb[3].setText("destroy");
+            l.setText("Question_9: Which of the following are true? ");
+            jb[0].setText("An array has a fixed size.");jb[1].setText("ArrayList is immutable.");jb[2].setText("Array is better than ArrayList");jb[3].setText("Arraylist i slow");
         }
         if(current==9)
         {
-            l.setText("Que10: Which one among these is not a valid component?");
-            jb[0].setText("JButton");jb[1].setText("JList");jb[2].setText("JButtonGroup");
-            jb[3].setText("JTextArea");
+            l.setText("Question_10: System.out.println(4 % 3); result?");
+            jb[0].setText("0");jb[1].setText("1");jb[2].setText("2");
+            jb[3].setText("3");
         }
         l.setBounds(30,40,450,20);
         for(int i=0,j=0;i<=90;i+=30,j++)
@@ -159,11 +163,11 @@ public  class OnlineTest extends JFrame implements ActionListener {
         if(current==1)
             return(jb[2].isSelected());
         if(current==2)
-            return(jb[3].isSelected());
-        if(current==3)
             return(jb[0].isSelected());
-        if(current==4)
+        if(current==3)
             return(jb[2].isSelected());
+        if(current==4)
+            return(jb[1].isSelected());
         if(current==5)
             return(jb[2].isSelected());
         if(current==6)
